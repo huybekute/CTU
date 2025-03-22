70b7c24c77a9dc87c6abf281025c6d1b6bd2a1de
your name <huynguyen.2764@gmail.com>
2025-03-22 16:37:39 +0700

    update

--- CT174 - PTTKTT/buoi4/CBL1QHD.c
+++ CT174 - PTTKTT/buoi4/CBL1QHD.c
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 
 typedef struct{
-    int TL, GT, SL, PA;
+    int TL, GT, PA;
     float DG;
     char Ten[50];
 } item;
 
 
 void readFile(item **a, int *n, int *m){
-    FILE *f = fopen("CBL2QHD.txt", "r");
+    FILE *f = fopen("CBL1QHD.txt", "r");
     if(f!=NULL){
         fscanf(f, "%d", m);
         int i = 0;
-        *a = (item*)malloc(sizeof(item));
+        (*a) = (item*)malloc(sizeof(item));
         while(!feof(f)){
             (*a) = (item*)realloc(*a, sizeof(item)*(i+1));
-            fscanf(f, "%d%d%d%[^\n]", &(*a)[i].TL, &(*a)[i].GT, &(*a)[i].SL, (*a)[i].Ten);
+            fscanf(f, "%d%d%[^\n]", &(*a)[i].TL, &(*a)[i].GT, (*a)[i].Ten);
             (*a)[i].DG = (float)(*a)[i].GT / (*a)[i].TL;
             (*a)[i].PA = 0;
             i++;
@@ -27,34 +27,30 @@ void readFile(item **a, int *n, int *m){
 }
 
 void print(item *a, int n){
-    int TTL = 0, TGT = 0;
+    int TGT = 0, TTL = 0;
     for(int i=0; i<n; i++){
-        printf("| %20s | %8d | %8d | %8.2f | %8d |\n", a[i].Ten, a[i].TL, a[i].GT, a[i].DG, a[i].PA);
-        TGT += a[i].GT * a[i].PA;
-        TTL += a[i].TL * a[i].PA;
+        printf("| %20s | %3d | %3d | %3.2f | %3d |\n", a[i].Ten, a[i].TL, a[i].GT, a[i].DG, a[i].PA);
+        TGT += a[i].PA * a[i].GT;
+        TTL += a[i].PA * a[i].TL;
     }
-    printf("TTL = %d\n", TTL);
     printf("TGT = %d\n", TGT);
+    printf("TTL = %d\n", TTL);
 }
 
 typedef int table[50][100];
 
-int min(int a, int b){
-    return a < b ? a : b;
-}
-
 void taoBang(item *a, int n, int m, table F, table X){
     for(int i=0; i<=m; i++){
-        X[0][i] = min(i/a[0].TL, a[0].SL);
-        F[0][i] = X[0][i] * a[i].GT;
+        X[0][i] = i/a[0].TL;
+        F[0][i] = X[0][i] * a[0].GT;
     }
-    for(int i=0; i<n; i++){
-        for(int j=0; j<=m ; j++){
+    for(int i=1; i<n; i++){
+        for(int j=0; j<=m; j++){
             F[i][j] = F[i-1][j];
             X[i][j] = 0;
-            for(int k=0; k<=min(j/a[i].TL, a[i].SL); k++){
+            for(int k=1; k<=(j/a[i].TL); k++){
                 if(k*a[i].GT + F[i-1][j-k*a[i].TL] > F[i][j]){
-                    F[i][j] = k *a[i].GT + F[i-1][j-k*a[i].TL];
+                    F[i][j] = k*a[i].GT + F[i-1][j-k*a[i].TL];
                     X[i][j] = k;
                 }
             }
@@ -74,7 +70,7 @@ void inBang(item *a, int n, int m, table F, table X){
 void traBang(item *a, int n, int m, table X){
     for(int i=n-1; i>=0; i--){
         a[i].PA = X[i][m];
-        m-=a[i].TL * X[i][m];
+        m -= X[i][m] * a[i].TL;
     }
 }
 
@@ -84,7 +80,7 @@ int main(){
     readFile(&a, &n, &m);
     table F, X;
     taoBang(a, n, m, F, X);
-    inBang(a, n, m, F, X);
-    traBang(a, n, m , X);
+    inBang(a, n, m, F ,X);
+    traBang(a, n, m, X);
     print(a, n);
 }
--- CT174 - PTTKTT/buoi4/CBL3QHD.c
+++ CT174 - PTTKTT/buoi4/CBL3QHD.c
@@ -2,23 +2,23 @@
 #include <stdlib.h>
 
 typedef struct{
-    int TL, GT, SL, PA;
+    int TL, GT, PA, SL;
     float DG;
     char Ten[50];
 } item;
 
-
 void readFile(item **a, int *n, int *m){
-    FILE *f = fopen("CBL2QHD.txt", "r");
+    FILE *f = fopen("CBL1QHD.txt", "r");
     if(f!=NULL){
         fscanf(f, "%d", m);
         int i = 0;
-        *a = (item*)malloc(sizeof(item));
+        (*a) = (item*)malloc(sizeof(item));
         while(!feof(f)){
             (*a) = (item*)realloc(*a, sizeof(item)*(i+1));
-            fscanf(f, "%d%d%d%[^\n]", &(*a)[i].TL, &(*a)[i].GT, &(*a)[i].SL, (*a)[i].Ten);
-            (*a)[i].DG = (float)(*a)[i].GT / (*a)[i].TL;
+            fscanf(f, "%d%d%[^\n]", &(*a)[i].TL, &(*a)[i].GT, (*a)[i].Ten);
+            (*a)[i].DG = (float)(*a)[i].GT/ (*a)[i].TL;
             (*a)[i].PA = 0;
+            (*a)[i].SL = 1;
             i++;
         }
         (*n) = i;
@@ -27,18 +27,18 @@ void readFile(item **a, int *n, int *m){
 }
 
 void print(item *a, int n){
-    int TTL = 0, TGT = 0;
+    int TGT = 0, TTL = 0;
     for(int i=0; i<n; i++){
-        printf("| %20s | %8d | %8d | %8.2f | %8d |\n", a[i].Ten, a[i].TL, a[i].GT, a[i].DG, a[i].PA);
-        TGT += a[i].GT * a[i].PA;
+        printf("| %20s | %3d | %3d | %3f | %3d | \n", a[i].Ten, a[i].TL, a[i].GT, a[i].DG, a[i].PA);
         TTL += a[i].TL * a[i].PA;
+        TGT += a[i].GT * a[i].PA;
     }
-    printf("TTL = %d\n", TTL);
     printf("TGT = %d\n", TGT);
+    printf("TTL = %d\n", TTL);
+
 }
 
 typedef int table[50][100];
-
 int min(int a, int b){
     return a < b ? a : b;
 }
@@ -46,15 +46,15 @@ int min(int a, int b){
 void taoBang(item *a, int n, int m, table F, table X){
     for(int i=0; i<=m; i++){
         X[0][i] = min(i/a[0].TL, a[0].SL);
-        F[0][i] = X[0][i] * a[i].GT;
+        F[0][i] = X[0][i] * a[0].GT;
     }
-    for(int i=0; i<n; i++){
-        for(int j=0; j<=m ; j++){
+    for(int i=1; i<n; i++){
+        for(int j=0; j<=m; j++){
             F[i][j] = F[i-1][j];
             X[i][j] = 0;
-            for(int k=0; k<=min(j/a[i].TL, a[i].SL); k++){
+            for(int k=1; k<=min(j/a[i].TL, a[i].SL); k++){
                 if(k*a[i].GT + F[i-1][j-k*a[i].TL] > F[i][j]){
-                    F[i][j] = k *a[i].GT + F[i-1][j-k*a[i].TL];
+                    F[i][j] = k*a[i].GT + F[i-1][j-k*a[i].TL];
                     X[i][j] = k;
                 }
             }
@@ -65,7 +65,7 @@ void taoBang(item *a, int n, int m, table F, table X){
 void inBang(item *a, int n, int m, table F, table X){
     for(int i=0; i<n; i++){
         for(int j=0; j<=m; j++){
-            printf("| %2d %2d ", F[i][j], X[i][j]);
+            printf("|%2d %2d ", F[i][j], X[i][j]);
         }
         printf("\n");
     }
@@ -74,7 +74,7 @@ void inBang(item *a, int n, int m, table F, table X){
 void traBang(item *a, int n, int m, table X){
     for(int i=n-1; i>=0; i--){
         a[i].PA = X[i][m];
-        m-=a[i].TL * X[i][m];
+        m-= X[i][m] * a[i].TL;
     }
 }
 
@@ -83,8 +83,8 @@ int main(){
     int n=0, m=0;
     readFile(&a, &n, &m);
     table F, X;
-    taoBang(a, n, m, F, X);
+    taoBang(a, n, m , F, X);
     inBang(a, n, m, F, X);
-    traBang(a, n, m , X);
+    traBang(a, n, m, X);
     print(a, n);
 }
